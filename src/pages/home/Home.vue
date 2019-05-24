633dd91cf6367d6b95fe0ae3fd55c266375e1988
chenwenhua666 <764471698@qq.com>
2019-05-24 08:53:24 +0800

    homepage

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,57 @@
+<template>
+ <div>
+   <home-header :city="city"></home-header>
+   <home-swiper :list="swiperList"></home-swiper>
+   <home-icons :list="iconList"></home-icons>
+   <home-recommend :list="recommendList"></home-recommend>
+   <home-weekend :list="weekendList"></home-weekend>
+ </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  data () {
+    return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      if (res.data.ret && res.data.data) {
+        this.city = res.data.data.city
+        this.swiperList = res.data.data.swiperList
+        this.iconList = res.data.data.iconList
+        this.recommendList = res.data.data.recommendList
+        this.weekendList = res.data.data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/readme.txt
@@ -0,0 +1,5 @@
+npm install fastclick --save
+npm install stylus --save
+npm install stylus-loader --save
+npm install vue-awesome-swiper@2.6.7 --save
+npm install axios --save
